46bc8f26085c247bf2c292c192b72f9cb4039d12
Wonderfall <wonderfall@targaryen.house>
2018-06-18 02:46:33 +0200

    oops

--- rootfs/usr/local/bin/run.sh
+++ rootfs/usr/local/bin/run.sh
@@ -3,6 +3,6 @@ sed -i -e "s/<UPLOAD_MAX_SIZE>/$UPLOAD_MAX_SIZE/g" /nginx/conf/nginx.conf /php/e
        -e "s/<MEMORY_LIMIT>/$MEMORY_LIMIT/g" /php/etc/php-fpm.conf
 
 [ ! "$(ls -A /organizr/api/config)" ] && cp /organizr/api/default.php /organizr/api/config/
-cp -r /organizr/plugins/images/tabs_tmp/* /organizr/plugins/images/tabs/
+cp -r /organizr/plugins/images/tabs_temp/* /organizr/plugins/images/tabs/
 chown -R $UID:$GID /organizr /nginx /php /tmp /etc/s6.d
 exec su-exec $UID:$GID /bin/s6-svscan /etc/s6.d
